26c6984d393ad79be6390ae6dbc51f9cc9484305
ogu83 <korogu@hotmail.com>
2016-03-18 11:07:36 +0200

    Components

---
+++ SD.UIElements/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SD.UIElements")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SD.UIElements")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("50cbc012-e3f2-402c-bd1a-b6373de4b06b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SD.UIElements/ResourcesWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using SilverDesigner.DOM;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SilverDesigner
+{
+    public partial class ResourcesWindow : ChildWindow
+    {
+        private ResourcesVM _myVM { get { return DataContext as ResourcesVM; } }
+
+        public ResourcesWindow()
+        {
+            InitializeComponent();
+        }
+        public ResourcesWindow(ResourcesVM dataContext)
+        {
+            DataContext = dataContext;
+            InitializeComponent();
+        }
+
+        private void OKButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            if (fd.ShowDialog().GetValueOrDefault())
+            {
+                byte[] data = File.ReadAllBytes(fd.File.FullName);
+                _myVM.AddResourceCommand(fd.File.Name, ResourceBase.Types.Image, data);
+            }
+        }
+
+        private void btnRemove_Click(object sender, RoutedEventArgs e)
+        {
+            _myVM.RemoveResourceCommand();
+        }
+    }
+}
+
